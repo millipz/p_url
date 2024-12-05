cdebb1dd6a8f313ca2621b9f6be2937a07e4cd9d
nc-milesphillips <184493025+nc-milesphillips@users.noreply.github.com>
2024-12-05 13:01:43 +0000

    remove dockerfile, comment out unused imports

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,11 @@
 import json
-from utils import shorten, get_url, write_url
+
+# from utils import shorten, get_url, write_url
 
 
 def lambda_handler(event, context):
     body = json.loads(event["body"])
+    print(body)
     method = event["httpMethod"]
     if method == "POST":
         return {"statusCode": 200, "body": json.dumps("url successfully stored")}
